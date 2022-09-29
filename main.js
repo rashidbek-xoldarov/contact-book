f7b0c9093e15d20f785d8049e171657ee955c591
rashidbek <rashidbek0309@gmail.com>
2022-09-30 00:12:23 +0500

    Add upload img

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ const elList = document.querySelector(".card-list");
 const elInputName = document.querySelector(".js-input-name");
 const elInputRel = document.querySelector(".js-input-rel");
 const elInputTel = document.querySelector(".js-input-tel");
+const elInputImg = document.querySelector(".js-input-img");
+
+const img = document.querySelector("img");
 
 const data = [];
+let src;
+elInputImg.addEventListener("change", (evt) => {
+  if (evt.target.files.length > 0) {
+    src = URL.createObjectURL(evt.target.files[0]);
+  }
+});
 
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
+  console.log(src);
   const valName = elInputName.value;
   const valRel = elInputRel.value;
   const valTel = elInputTel.value;
@@ -17,6 +27,7 @@ elForm.addEventListener("submit", function (evt) {
     name: valName,
     reletion: valRel,
     tel: valTel,
+    url: src,
   };
 
   const item = data.find((item) => item.tel == valTel);
@@ -41,9 +52,12 @@ function renderUi(arr) {
     //create Element
     const newLi = document.createElement("li");
     const title = document.createElement("h3");
+    const fistWrapper = document.createElement("div");
+    const secondWrapper = document.createElement("div");
     const text = document.createElement("p");
     const link = document.createElement("a");
     const delBtn = document.createElement("button");
+    const img = document.createElement("img");
 
     //set Atribute
     title.textContent = item.name;
@@ -52,6 +66,10 @@ function renderUi(arr) {
     link.href = `tel: ${item.tel}`;
     delBtn.dataset.delId = item.id;
     delBtn.textContent = "Delete";
+    img.src = `${item.url}`;
+    img.width = "60";
+    img.height = "60";
+    img.style.borderRadius = "50%";
     newLi.setAttribute("class", "card-item");
     title.setAttribute("class", "card-item-title");
     text.setAttribute("class", "card-text");
@@ -60,10 +78,13 @@ function renderUi(arr) {
 
     //Appendchild
 
-    newLi.appendChild(title);
-    newLi.appendChild(text);
-    newLi.appendChild(link);
-    newLi.appendChild(delBtn);
+    fistWrapper.appendChild(img);
+    fistWrapper.appendChild(delBtn);
+    secondWrapper.appendChild(title);
+    secondWrapper.appendChild(text);
+    secondWrapper.appendChild(link);
+    newLi.appendChild(fistWrapper);
+    newLi.appendChild(secondWrapper);
 
     elList.appendChild(newLi);
 
